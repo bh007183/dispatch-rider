f3d00ca9153441df12abf8ceb5678cb27562d616
Ben Hopkins <bjhops17@gmail.com>
2021-04-03 15:03:22 -0700

    participents now listed in state

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
-import "./style.css"
+import { GlobalContext } from "../../globalContext";
+import "./style.css";
+import { Link } from "react-router-dom";
 
-export default function Message() {
-
-
-    const clickermiclicking = event => {
-        event.stopPropagation()
-        console.log(event.currentTarget.value)
-    }
-  return (
-    <button value="test" style={{width: "100%"}} className="button" onClick={clickermiclicking} >
-    <Grid container className={"eachMessage"} item >
-      <Grid item className={"profileImg"} xs={4}>
-        <div className={"foundation"}>
-          <div className={"midlayer"}>
-          </div>
-        </div>
-      </Grid>
-      <Grid className="recent" item xs={8}>
-        
-
-      </Grid>
+export default function Message(props) {
+  const { global, setGlobal } = useContext(GlobalContext);
+  const clickermiclicking = event => {
+      event.stopPropagation()
       
-    </Grid>
-    </button >
-   
+      setGlobal({...global, participants: JSON.parse(event.currentTarget.attributes[0].value)})
+
+ 
+  }
+  return (
+    <Link
+      value={props.participants}
+      to="/conversation"
+      onClick={clickermiclicking}
+      style={{ width: "100%" }}
+      className="button"
+      
+    >
+      <Grid container className={"eachMessage"} item>
+        <Grid item className={"profileImg"} xs={4}>
+          <div className={"foundation"}>
+            <div className={"midlayer"}></div>
+          </div>
+        </Grid>
+        <Grid className="recent" item xs={8}>
+          {props.message}
+        </Grid>
+      </Grid>
+    </Link>
   );
 }
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,72 +1,103 @@
 import Message from "../../components/message";
-import React, { useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { GlobalContext } from "../../globalContext";
-import "./style.css"
-import {Link} from "react-router-dom"
+import "./style.css";
+import { Link } from "react-router-dom";
 
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
 
-
-
 export default function Main() {
-    const { global, setGlobal } = useContext(GlobalContext);
+  const { global, setGlobal } = useContext(GlobalContext);
 
-    useEffect(() => {
-        axios.get("http://localhost:8080/friends", {
-                headers: { authorization: "Bearer: " + localStorage.getItem("Auth") },
-              }).then(res => setGlobal({...global, friends: res.data}))
-        axios.get("http://localhost:8080/conversation", {
-                headers: { authorization: "Bearer: " + localStorage.getItem("Auth") },
-              }).then(res => console.log(res))
-              // setGlobal({...global, conversations: res.data})
-        
-    }, [])
-    const clickermiclicking = event => {
-        setGlobal({...global, participants: [event.currentTarget.attributes[1].value, localStorage.getItem("UserId")]})
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/conversation", {
+        headers: { authorization: "Bearer: " + localStorage.getItem("Auth") },
+      })
+      .then((res) => setGlobal({ ...global, conversations: res.data }));
+  }, [global.conversations.length]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/friends", {
+        headers: { authorization: "Bearer: " + localStorage.getItem("Auth") },
+      })
+      .then((res) => setGlobal({ ...global, friends: res.data }));
+  }, [global.friends.length]);
 
-    }
+  const clickermiclicking = (event) => {
+    setGlobal({
+      ...global,
+      participants: [
+        event.currentTarget.attributes[1].value,
+        localStorage.getItem("UserId"),
+      ],
+    });
+  };
 
-  
+  const otherClick = (event) => {
+    console.log(event.currentTarget)
+  };
 
   return (
     <>
       <Grid className="participants" container>
-        {global.friends.length === 0 ? (<></>):(global.friends.map((friend) =>
-          friend.isOnline ? (
-            
-            <Grid key={friend.id} item className={"profileImgPar"} xs={3}>
-              <Link to="/conversation" className="button" value={friend.id} onClick={clickermiclicking} >
-              <div xs={12} style={{backgroundColor: "green"}} className={"foundationPar"}>
-                <div className={"midlayerPar"}></div>
-                
-                
-              </div>
-              <h6 className="name" direction='column' item xs={12}>{friend.firstName}</h6>
-              
-              </Link>
+        {global.friends.length === 0 ? (
+          <></>
+        ) : (
+          global.friends.map((friend) =>
+            friend.isOnline ? (
+              <Grid key={friend.id} item className={"profileImgPar"} xs={3}>
+                <Link
+                  to="/conversation"
+                  className="button"
+                  value={friend.id}
+                  onClick={clickermiclicking}
+                >
+                  <div
+                    xs={12}
+                    style={{ backgroundColor: "green" }}
+                    className={"foundationPar"}
+                  >
+                    <div className={"midlayerPar"}></div>
+                  </div>
+                  <h6 className="name" direction="column" item xs={12}>
+                    {friend.firstName}
+                  </h6>
+                </Link>
               </Grid>
-            
-          ) : (
-           
-            <Grid key={friend.id} item className={"profileImgPar"} xs={3}>
-              <Link to="/conversation" className="button" value={friend.id} onClick={clickermiclicking} >
-              <div xs={12} style={{backgroundColor: "red"}} className={"foundationPar"}>
-                <div className={"midlayerPar"}></div>
-                
-                
-              </div>
-              <h6 className="name" direction='column' item xs={12}>{friend.firstName}</h6>
-              
-              </Link>
+            ) : (
+              <Grid key={friend.id} item className={"profileImgPar"} xs={3}>
+                <Link
+                  to="/conversation"
+                  className="button"
+                  value={friend.id}
+                  onClick={clickermiclicking}
+                >
+                  <div
+                    xs={12}
+                    style={{ backgroundColor: "red" }}
+                    className={"foundationPar"}
+                  >
+                    <div className={"midlayerPar"}></div>
+                  </div>
+                  <h6 className="name" direction="column" item xs={12}>
+                    {friend.firstName}
+                  </h6>
+                </Link>
               </Grid>
-           
-          
-        
+            )
           )
-        ))}
+        )}
       </Grid>
-      <Message />
+      {global.conversations.map((group) => (
+        <Message
+          key={group.participants}
+          message={group.message}
+          participants={group.participants}
+          
+        />
+      ))}
     </>
   );
 }
